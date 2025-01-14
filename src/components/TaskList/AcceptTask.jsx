39558808d1e51535c47be889852058a02902c5d0
hrsh-1294 <harshvashishth12@gmail.com>
2025-01-14 23:03:17 +0530

    formatting

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -4,19 +4,19 @@ const AcceptTask = ({data}) => {
   
   return (
     <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl '>
-    <div className='flex justify-between items-center '>
-        <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-        <h4 className='text-sm'>{data.task_date}</h4>
+      <div className='flex justify-between items-center '>
+          <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+          <h4 className='text-sm'>{data.task_date}</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'> {data.task_title}</h2>
+      <p className='text-sm mt-2 '>
+        {data.task_description}
+      </p>
+      <div className='flex justify-between mt-8'>
+          <button className='bg-green-500 py-1 px-2 text-sm'>Mark as Completed</button>
+          <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button>
+      </div>
     </div>
-    <h2 className='mt-5 text-2xl font-semibold'> {data.task_title}</h2>
-    <p className='text-sm mt-2 '>
-       {data.task_description}
-    </p>
-    <div className='flex justify-between mt-8'>
-        <button className='bg-green-500 py-1 px-2 text-sm'>Mark as Completed</button>
-        <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button>
-    </div>
-</div>
 
   )
 }
--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const FailedTask = ({data}) => {
   return (
     <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl '>
-            <div className='flex justify-between items-center '>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-                <h4 className='text-sm'>{data.task_date}</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'> {data.task_title}</h2>
-            <p className='text-sm mt-2 '>
-            {data.task_description}
-            </p>
-            <div className='mt-12'>
-            <button className='w-full'>Failed</button>
-            </div>
+        <div className='flex justify-between items-center '>
+          <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+          <h4 className='text-sm'>{data.task_date}</h4>
         </div>
+      <h2 className='mt-5 text-2xl font-semibold'> {data.task_title}</h2>
+      <p className='text-sm mt-2 '>
+      {data.task_description}
+      </p>
+      <div className='mt-12'>
+      <button className='w-full bg-red-600'>Failed</button>
+      </div>
+    </div>
   )
 }
 
